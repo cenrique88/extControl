450d1d6df5c04f8788e1ed9e30f92b4820bb502a
cenrique88 <cenrique88.uy@gmail.com>
2025-04-15 11:56:16 -0300

    cambiando el esquema de reviciones

--- server/models/Revisiones.js
+++ server/models/Revisiones.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const extintorSchema = new mongoose.Schema({
     fecha_revision: { type: Date, required: true },
     id_extintor: { type: String, required: true },
+    estado_anterior:{type: Object, required: true},
 
 });
 
