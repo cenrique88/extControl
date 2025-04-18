11d3858f698a5bfa10317e04ba9787a44134c55d
cenrique88 <cenrique88.uy@gmail.com>
2025-04-18 13:21:25 -0300

    cambio de la bse de datos

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb://localhost:27017/myDatabase";
+const url = "mongodb://localhost:27017/database";
 
 const connectDB = async () => {
 	try {
