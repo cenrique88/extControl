cd25fa91dd0dfd5b25e2419014674f4f29b61516
cenrique88 <cenrique88.uy@gmail.com>
2025-04-24 22:13:38 -0300

    start neww

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,7 +18,16 @@ const Card = (props) => {
 
   return (
     <div className="card"> 
-      
+
+    <div className="id">ID</div>
+    <div className='ubicacion'>ubicacion</div>
+    <div className='tipo'>tipo</div>
+    <div className='capacidad'>capacidad</div>
+    <div className='recarga-cada'>recarga</div>
+    <div className='ultima-recarga'>recarga</div>
+    <div className='fecha-vencimiento'>vencimiento</div>
+    <div className='tiempo-faltante'>faltante</div>   
+
     </div>
   )
 }
