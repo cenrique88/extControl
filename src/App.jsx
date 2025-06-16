b6280c3f6cc130c576489fa1251b3ff259805802
Richard Rodriguez <richard.electronica2014@gmail.com>
2025-06-16 00:37:07 -0300

    Cambios en los estilos globales y responsivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import "./components/styles/App.css";
+//Estilos globales y responsivos
+import './components/styles/global.css';      
+import './components/styles/responsive.css';  
+import './components/styles/App.css';
 
 //Dependencias de las paginas de rutas...
 import Home from "./components/Home";
