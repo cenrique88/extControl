c6b8a1de546554bb6088fff14f28d5b04a394561
Richard Rodriguez <richard.electronica2014@gmail.com>
2025-06-17 01:53:57 -0300

    Cambios en el Home y NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,54 +1,40 @@
 
 import { useState } from "react";
 import Menu from "./Menu";
-import "./styles/App.css";
-
+import "./styles/NavBar.css"; // Reemplaza App.css por un estilo dedicado
 
 function NavBar() {
-	const [isOpen, setIsOpen] = useState(false);
-	const [selectedMenu, setSelectedMenu] = useState('Home');
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedMenu, setSelectedMenu] = useState("Home");
 
+  const onSelectMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-	const onSelectMenu = () => {
-		setIsOpen(!isOpen);
-	}
-	
+  return (
+  <>
+    <nav>
+      <img
+        id="menu_button"
+        tabIndex="0"
+        className={`menu-button ${isOpen ? "open" : ""}`}
+        src="/src/img/menu_white.png"
+        alt="Menu"
+        onBlur={() => setIsOpen(false)}
+        onClick={onSelectMenu}
+      />
+      <span className="nav-title">{selectedMenu}</span>
+    </nav>
 
-	
-
-	return (
-		<nav style={{height: "50px"}}>
-
-			
-			<ul>
-				<li>
-					<img id='menu_button' 
-					tabIndex="0"
-					className={`menu-button ${isOpen ? "open" : ""}`}
-					src='/src/img/menu_white.png' 
-					alt='Menu' 
-					onBlur={() => setIsOpen(false)}
-					onClick={onSelectMenu} />
-
-				</li>
-				<li>
-					{selectedMenu}					
-				</li>
-				<li>
-					<Menu isOpen={isOpen} setIsOpen={setIsOpen} setSelectedMenu={setSelectedMenu}/>
-				</li>
-
-
-
-				{/*
-					<Link id='extintores' to='/showext' onClick={(e) => onSelectMenu(e)}>
-						<img id='extintores' className='menu-img' src={menuExtintor} alt='Home' />
-						Extintor
-					</Link>
-				</li> */}
-			</ul>
-		</nav>
-	);
+    {/* Este menú tiene que estar afuera del <nav> */}
+    <Menu
+      isOpen={isOpen}
+      setIsOpen={setIsOpen}
+      setSelectedMenu={setSelectedMenu}
+    />
+  </>
+);
 }
 
 export default NavBar;
+
