0fcb83a4c8ba13cfc96740fa1ae084de9c60580c
cenrique88 <cenrique88.uy@gmail.com>
2025-06-27 00:00:21 -0300

    creacion de un componente y una ruta para add clliente para eliminar el modal

--- src/components/AddNewClient.jsx
+++ src/components/AddNewClient.jsx
@@ -1,4 +1,8 @@
-
+import {useState} from "react";
+import "./styles/Clientes.css"
+import useDataBase from "./hooks/useDataBase";
+import useForm from "./hooks/useForm";
+import Notify from "./Notify";
 
 
 
@@ -6,11 +10,67 @@
 
 const AddNewClient = () => {
 
+  const {writeDB} = useDataBase();
+  const nombre_juridico = useForm();
+  const direccion = useForm();
+  const nombre_cliente = useForm();
+  const email = useForm();
+
+
+  const saveData = () => {
+    const data = {nombre_cliente:nombre_cliente.inputValue, 
+                  email:email.emailValue,
+                  nombre_juridico:nombre_juridico.inputValue,
+                  direccion:direccion.inputValue
+                }
+    const rtrn = writeDB("clientes/add-client", data);
+    console.log({rtrn})
+    setShowNotify(true)
+    nombre_cliente.clearInput();
+    email.clearEmail();
+    nombre_juridico.clearInput();
+    direccion.clearInput();
+  }
+
+  const [showNotify, setShowNotify] = useState(false);
+  const onCloseNotify = ()=>{
+    setShowNotify(false)
+  }
+
 
 
 
   return (
     <div>
+      <Notify msg="Cliente Guardado" open={showNotify} close={onCloseNotify}/>
+        <input 
+            type="text" 
+            placeholder="Nombre Juridico: "
+            value={nombre_juridico.inputValue}
+            onChange={nombre_juridico.handleChangeInput}
+        ></input>   
+        <input 
+            type="text" 
+            placeholder="Nombre: "
+            value={nombre_cliente.inputValue}
+            onChange={nombre_cliente.handleChangeInput}
+        ></input>
+        <input 
+            type="email" 
+            placeholder="E-mail"
+            value={email.emailValue}
+            onChange={email.handleChangeEmail}
+        ></input>
+        <input 
+            type="text" 
+            placeholder="Direccion:"
+            value={direccion.inputValue}
+            onChange={direccion.handleChangeInput}
+        ></input>
+
+        <button
+        onClick={saveData}
+        >Guardar</button>
       
     </div>
   )
