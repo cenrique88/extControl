8b7bb2da34f5d081924cf63c0fefd7f36286c5ac
cenrique88 <cenrique88.uy@gmail.com>
2025-07-05 00:26:09 -0300

    soluciones de errores de starter

--- src/components/Clientes.jsx
+++ src/components/Clientes.jsx
@@ -13,6 +13,8 @@ import { Link } from "react-router-dom";
 import {AppContext} from "./AppContext";
 
 
+const Clientes = () => {
+
 
   const {setSelectedPage} = useContext(AppContext);
   const {getDB, deleteDB}= useDataBase()
