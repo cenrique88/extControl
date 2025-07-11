03ca9e9ad07d39bf5160ea9d964d2fa0242a1b5b
Richard Rodriguez <richard.electronica2014@gmail.com>
2025-07-10 21:33:53 -0300

    Creacion de Card nueva para Extintores.

--- src/extintores/components/Extintor.jsx
+++ src/extintores/components/Extintor.jsx
@@ -1,7 +1,6 @@
 import "../styles/Extintores.css";
 
-import {useState, useEffect, useContext} from 'react';
-
+import { useState, useEffect, useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router";
 
@@ -12,121 +11,108 @@ import Notify from "../../app/components/Notify";
 import AppContext from "../../app/components/AppContext";
 import useDate from "../../hooks/useDate";
 
-
-
 const Extintor = () => {
-const navigate = useNavigate();
-const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const [showAddExt, setShowAddExt] = useState(false);
-  const [getDataExtintor, setDataExtintor] = useState(null);
+  const [getDataExtintor, setDataExtintor] = useState([]);
   const [showNotify, setShowNotify] = useState(false);
   const [msgNotify, setMsgNotify] = useState("");
-  const {setSelectedPage} = useContext(AppContext)
+  const { setSelectedPage, selectedClient } = useContext(AppContext);
 
   const fv = useDate();
-  const {writeDB, getDB, deleteDB} = useDataBase();
-
+  const { writeDB, getDB, deleteDB } = useDataBase();
 
   const openModalAddExtintor = (prop) => {
-    if(prop){
-      document.getElementById('add-button-ext').style.visibility = "hidden";
-    }
-    else{
-      document.getElementById('add-button-ext').style.visibility = "visible";
-    }
+    document.getElementById('add-button-ext').style.visibility = prop ? "hidden" : "visible";
     setShowAddExt(prop);
-  }
+  };
 
-  const onCloseNotify = ()=>{
-    setShowNotify(false)
-  }
+  const onCloseNotify = () => {
+    setShowNotify(false);
+  };
 
   useEffect(() => {
-      setSelectedPage(location.pathname === '/extintores' ? 'Extintores' : '');
-      getAllExtintor();
-    }, [])
+    setSelectedPage(location.pathname === '/extintores' ? 'Extintores' : '');
+    getAllExtintor();
+  }, []);
 
   const getAllExtintor = async () => {
-		const data = await getDB("extintores");
-    const temp = [];
-    if(data){
-      data.map((extintor)=>{extintor.cliente == selectedClient ? temp.push(extintor) : ''})
+    const data = await getDB("extintores");
+    if (data) {
+      const temp = data.filter((extintor) => extintor.cliente === selectedClient);
+      setDataExtintor(temp);
     }
-
-	}
+  };
 
   const saveExtintor = async (data_ext) => {
-        const ext = await writeDB("extintores", data_ext);
+    await writeDB("extintores", data_ext);
+    getAllExtintor();
+    setMsgNotify("Extintor guardado");
+    setShowNotify(true);
+    setShowAddExt(false);
+  };
+
+  const deleteExtintor = async (id) => {
+    const alerta = confirm("Esta eliminando un extintor, ¿desea continuar?");
+    if (alerta) {
+      const response = await deleteDB("extintores", id);
+      if (response) {
         getAllExtintor();
-        setMsgNotify("Extintor guardado");
+        setMsgNotify("Extintor eliminado");
         setShowNotify(true);
+      }
     }
+  };
 
-    const deleteExtintor = async (id) => {
-      const alerta = confirm("Esta eliminando un extintor, ¿desea continuar?");
-      if(alerta){
-        const response = await deleteDB("extintores", id);
-        if(response){
-          getAllExtintor();
-          setMsgNotify("Extintor eliminado");
-          setShowNotify(true);
-        }
-      }      
-    }
+  const editExtintor = (id, data) => {
+    console.log("edit", id);
+  };
 
-    const editExtintor = (id, data) => {
-      console.log("edit")
-    }
+  const handleF_Vencimiento = (ultima_recarga, recarga_cada) => {
+    const fv = new Date(ultima_recarga);
+    const rc = parseInt(recarga_cada);
+    if (isNaN(rc)) return "-";
+    fv.setFullYear(fv.getFullYear() + rc);
+    return `${(fv.getMonth() + 1).toString().padStart(2, '0')}/${fv.getFullYear()}`;
+  };
 
-    // funcion para obtener la fecha de vencimiento de un extintor
-    const handleF_Vencimiento = (ultima_recarga, recarga_cada) => {
-      const fv = new Date(ultima_recarga);
-      const rc = parseInt(recarga_cada);
-      const getTime= fv.setFullYear(fv.getFullYear()+  rc)     
-      return `${new Date(getTime).getMonth()}/${new Date(getTime).getFullYear()%1000}`;
-    }
-
-
-
-
-
-
-    
   return (
     <div>
-      <Notify msg={msgNotify} open={showNotify} close={onCloseNotify}/>
-      <button id="add-button-ext" 
-              className="add-button" 
-              onClick={()=>openModalAddExtintor(true)}>
-            +
+      <Notify msg={msgNotify} open={showNotify} close={onCloseNotify} />
+
+      <button id="add-button-ext" className="add-button" onClick={() => openModalAddExtintor(true)}>
+        +
       </button>
 
       <input
-      className="search-bar" 
+        className="search-bar"
         type="text"
-        placeholder="implementacion de la busqueda por filtro!!!"/>  
+        placeholder="¡Implementación de la búsqueda por filtro próximamente!"
+      />
 
+      {showAddExt && (
+        <FormExtintor getDB={getDB} saveExtintor={saveExtintor} />
+      )}
 
-        <div className="scroll-container">
-          {
-            getDataExtintor && getDataExtintor.map((ext) => (<ExtintorCard 
-                                                              key={ext._id} 
-                                                              extintor={ext} 
-                                                              deleteExtintor={deleteExtintor} 
-                                                              editExtintor={editExtintor} 
-                                                              handleF_Vencimiento={fv.handleF_Vencimiento}
-                                                              />))
-          }
-        </div>
-          <div className="footer-select">
-          aqui se implementara la seleccion de extintores por letras
+      <div className="scroll-container">
+        {getDataExtintor.map((ext) => (
+          <ExtintorCard
+            key={ext._id}
+            extintor={ext}
+            deleteExtintor={deleteExtintor}
+            editExtintor={editExtintor}
+            handleF_Vencimiento={handleF_Vencimiento}
+          />
+        ))}
+      </div>
 
-        </div>
-
-
+      <div className="footer-select">
+        Aquí se implementará la selección de extintores por letras
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Extintor
+export default Extintor;
